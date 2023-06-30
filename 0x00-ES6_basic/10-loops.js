3c135092708367c9fa30cda9de556744549a9a31
oyebamiji mustapha <oyebamijimustapha44@gmail.com>
2023-06-30 17:43:08 +0100

    feat: update the for of loop

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,6 +1,6 @@
 export default function appendToEachArrayValue(array, appendString) {
-  let array2 = [];
-  for (var idx of array) {
+  const array2 = [];
+  for (let idx of array) {
     array2.push(appendString + idx);
   }
   return array2;
