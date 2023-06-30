d8289a459136ef67f3996d7e6f1fef41969189d0
Musoye <oyebamijimustapha44@gmail.com>
2023-06-30 18:17:00 +0100

    feat: eslint fixing style

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,6 +1,6 @@
 export default function appendToEachArrayValue(array, appendString) {
   const array2 = [];
-  for (let idx of array) {
+  for (const idx of array) {
     array2.push(appendString + idx);
   }
   return array2;
--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,8 +1,8 @@
 export default function createReportObject(employeesList) {
-    return {
-      allEmployees: { ...employeesList },
-      getNumberOfDepartments(employ) {
-        return Object.keys(employ).length;
-      }
-    };
+  return {
+    allEmployees: { ...employeesList },
+    getNumberOfDepartments(employ) {
+      return Object.keys(employ).length;
+    },
+  };
 }
