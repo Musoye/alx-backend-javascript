1f9864f613ca3beb18338e9a9523dd9a55aeb864
oyebamiji mustapha <oyebamijimustapha44@gmail.com>
2023-06-30 14:39:30 +0100

    feat: A Building

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,16 @@
+export default class Building {
+    constructor(sqft) {
+        if (this.constructor !== Building && !this.evacuationWarningMessage) throw new Error('Class extending Building must override evacuationWarningMessage')
+        if (typeof sqft !== 'number') throw new TypeError('Sqft must be a Number');
+        this._sqft = sqft;
+    }
+
+    get sqft() {
+        return this._sqft;
+    }
+
+    set sqft(newSqft) {
+        if (typeof newSqft !== number) throw new TypeError('Sqft must be a Number');
+        this._sqft = newSqft;
+    }
+}
