137d917fb41b99576e64bb059660a28ea7dfef49
oyebamiji mustapha <oyebamijimustapha44@gmail.com>
2023-06-30 09:21:31 +0100

    feat:  Methods, static methods, computed methods names..... MONEY

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,36 @@
+export default class HolbertonCourse {
+  constructor(code , name) {
+    if (typeof name !== 'string') throw new TypeError('Name must be a String');
+    if (typeof code !== 'number') throw new TypeError('Code must be a String');
+    this._code = code; 
+    this._name = name;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(newName) {
+    if (typeof newName === 'string') {
+      this._name = newName;
+    } else {
+      throw new TypeError('Name must be a String');
+    }
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(newLength) {
+    if (typeof newLength === 'string') {
+      this._code  = newLength;
+    } else {
+      throw new TypeError('Code must be a String');
+    }
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})}`
+  }
+}
