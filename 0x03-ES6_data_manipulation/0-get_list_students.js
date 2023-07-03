d2790c38b677552556c96fd98a6b9a6173c70c2e
Musoye <oyebamijimustapha44@gmail.com>
2023-07-03 21:57:52 +0100

    feat: mapping

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -2,6 +2,6 @@ export default function getListStudentIds() {
   return ([
     { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
     { id: 2, firstName: 'James', location: 'Columbia' },
-    { id: 5, firstName: 'Serena', location: 'San Francisco' }
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
   ]);
 }
